80f5e7cdb2ccfdc9b9440325cc7290dff51728a5
pupanya <pupanya@mail.ru>
2022-06-06 00:17:55 +0500

    module 3 task 2. add invert function

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -3,6 +3,12 @@
  * @param {object} obj - the initial object
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
-export function invertObj(obj) {
+export function invertObj(obj) {;
+  if (!obj) return;
 
+  const result = {}
+  for (const [key, value] of Object.entries(obj)) {
+    result[value] = key;
+  }
+  return result;
 }
