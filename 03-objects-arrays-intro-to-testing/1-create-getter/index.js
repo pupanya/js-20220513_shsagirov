724a40c99b75ff9d65741a1eef8a29a8818b93d2
pupanya <pupanya@mail.ru>
2022-06-07 16:50:25 +0500

    module 3 task 1. create getter function

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const arr = path.split('.');
+  return (value) => {
+    let result = value;
+    for (const part of arr) {
+      if (!result) {
+        break;
+      }
+      result = result[part];
+    }
+    return result;
+  };
 }
